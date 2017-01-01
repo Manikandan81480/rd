a8093f4d352f3f96eb6e39f9e964d7c72910e8c8
Manikandan81480 <mani81480@gmail.com>
2017-01-01 02:44:47 -0800

    Update and rename README.md to reversedigit.java

---
+++ reversedigit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class reversedigit
+{
+public static void main(Strings...args)
+{
+Scanner x=new Scanner(System.in);
+int z=x.nextInt();
+int a=0,b=0;
+while(z!=0)
+{
+a=z%10;
+z=z/10;
+System.out.println(a);
+}
+}
+}
